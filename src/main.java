2dd69248db83471c22df8ef223bc22b23e284125
unknown <bazo20061996@gmail.com>
2022-07-24 23:15:09 +0700

    MD2-bai1-tap khai bao bien

---
+++ src/main.java
@@ -0,0 +1,12 @@
+public class main {
+    public static void main(String[] args) {
+        int soNguyen = 10;
+        float soThuc = 10.5;
+        double soThuc2 = 10.6;
+        boolean dungSai = false;
+        char kieuChu = 'a';
+        String chuoi = "tran van ba";
+        System.out.println("so nguyen la " + soNguyen);
+
+    }
+}
